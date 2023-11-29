5e3a24742ec1b20c326eb32bcf12b35aebd9b2a1
MichalLekacz <87401492+MichalLekacz@users.noreply.github.com>
2023-11-29 20:33:38 +0000

    updated html and js

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-
 // Ścieżka do pliku JSON lokalnie
-const filePath = './advanced-js.json';
+const filePath = "./advanced-js.json";
 
 // Użyj Fetch do pobrania danych
 fetch(filePath)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(jsonData => {
-        console.log(jsonData);
- // Znajdź element z klasą "container"
- var containerElement = document.querySelector('.container');
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((jsonData) => {
+    console.log(jsonData);
+    // Znajdź element z klasą "container"
+    var containerElement = document.querySelector(".container");
 
- // Tworzenie elementów HTML z danymi JSON za pomocą forEach
- jsonData.forEach(function(item) {
-     // Tworzenie elementu article
-     var articleElement = document.createElement('article');
-     articleElement.classList.add('article');
+    // Tworzenie elementów HTML z danymi JSON za pomocą forEach
+    jsonData.forEach(function (item) {
+      // Tworzenie elementu article
+      var articleElement = document.createElement("article");
+      articleElement.classList.add("article");
 
-     // Tworzenie elementu h2
-     var h2Element = document.createElement('h2');
-     h2Element.textContent = item.title;
+      // Tworzenie elementu h2
+      var h2Element = document.createElement("h2");
+      h2Element.textContent = item.title;
 
-     // Tworzenie elementu p
-     var pElement = document.createElement('p');
-     pElement.textContent = item.article;
+      // Tworzenie elementu p
+      var pElement = document.createElement("p");
+      pElement.textContent = item.article;
 
-     // Tworzenie elementu pre i code
-     var preElement = document.createElement('pre');
-     var codeElement = document.createElement('code');
-     preElement.classList.add('hljs');
-     codeElement.classList.add('javascript');
-     codeElement.classList.add('hljs');
-     codeElement.classList.add('language-javascript');
-     codeElement.setAttribute('data-highlighted', 'yes');
-     codeElement.textContent = item.code;
-     preElement.appendChild(codeElement);
+      // Tworzenie elementu pre i code
+      var preElement = document.createElement("pre");
+      var codeElement = document.createElement("code");
+      //   preElement.classList.add("hljs");
+      codeElement.classList.add("javascript");
+      codeElement.classList.add("hljs");
+      codeElement.classList.add("language-javascript");
+      //   codeElement.setAttribute("data-highlighted", "yes");
+      codeElement.textContent = item.code;
+      preElement.appendChild(codeElement);
 
-     // Dodawanie elementów do article
-     articleElement.appendChild(h2Element);
-     articleElement.appendChild(pElement);
-     articleElement.appendChild(preElement);
+      // Dodawanie elementów do article
+      articleElement.appendChild(h2Element);
+      articleElement.appendChild(pElement);
+      articleElement.appendChild(preElement);
 
-     // Dodawanie article do container
-     containerElement.appendChild(articleElement);
- });
-    })    
-    .catch(error => {
-        console.error('Błąd pobierania pliku JSON:', error);
+      // Dodawanie article do container
+      containerElement.appendChild(articleElement);
     });
+  })
+  .catch((error) => {
+    console.error("Błąd pobierania pliku JSON:", error);
+  })
+  .finally(() => {
+    hljs.highlightAll();
+  });
